8aebd324c75140f2aca9953f84e8a101ae3cacc7
Francesco Strozzi <francesco.strozzi@gmail.com>
2011-02-21 15:17:20 +0100

    changed Options class into NativeOptions

--- lib/bio-ngs/options.rb
+++ lib/bio-ngs/options.rb
@@ -1,11 +1,13 @@
 module Bio
   module NGS
-    class Options
+    class NativeOptions
       
-      # simple parsing from ARGV to bypass Thor options parsing and 
+      attr_reader :task_name, :args
+      # simple parsing from ARGV used to bypass Thor options parser and 
       # let the user call a tool with his own native set of parameters
-      def self.use_native_options
-        return  ARGV[0], ARGV.slice(1..-1) 
+      def initialize(arguments=ARGV)
+          @task_name = arguments[0]
+          @args = arguments[1..-1].flatten!
       end
       
     end
