900a53fc2e286d72d2a3967f59bb99164ba4262f
Valeria Ranzani <ranzanivaleria@macbook-pro-ranzani.ingm.ad>
2011-02-25 12:32:16 +0100

    Implement qseq2fastq PE and SE

---
+++ lib/bio/ngs.rb
@@ -0,0 +1,15 @@
+module Bio
+  class Ngs
+    module Converter
+      def self.qseq2fastq_pe(qseq_line)
+        qseq = qseq_line.split #logic here
+        qseq_out = "@#{qseq[0]}:#{qseq[2]}:#{qseq[3]}:#{qseq[4]}:#{qseq[5]}#0/#{qseq[7]}\n#{qseq[8]}\n+\n#{qseq[9].gsub(/N/,'\.')}" if qseq[10]=="1"
+      end
+      def self.qseq2fastq_se(qseq_line)
+        qseq = qseq_line.split #logic here
+        qseq_out = "@#{qseq[0]}:#{qseq[2]}:#{qseq[3]}:#{qseq[4]}:#{qseq[5]}#0/\n#{qseq[8]}\n+\n#{qseq[9].gsub(/N/,'\.')}" if qseq[10]=="1"
+      end
+    end #Formatter
+  end #Ngs
+end #Bio
+
---
+++ test/test_ngs.rb
@@ -0,0 +1,22 @@
+require 'helper'
+
+class TestNgs < Test::Unit::TestCase
+  # should "probably rename this file and start testing for real" do
+  #   flunk "hey buddy, you should probably rename this file and start testing for real"
+  # end
+  def setup
+    @qseq_str="HWI-BRUNOP16X   0001    8       1       15744   1220    0       1       GNAGCCGATCCACCTCCCAGCCTTCCTGGGATACAAGTCTGGCATGACTC      TBTTTTTTTT`^``Sdbd_deeeeeeeeeeeWUdd_bbbfdTedXOTTTS      1"
+  end
+  
+  def test_qseq2fastq_pe
+    qseq_pe_output="@HWI-BRUNOP16X:8:1:15744:1220#0/1\nGNAGCCGATCCACCTCCCAGCCTTCCTGGGATACAAGTCTGGCATGACTC\n+\nTBTTTTTTTT`^``Sdbd_deeeeeeeeeeeWUdd_bbbfdTedXOTTTS"    
+    assert_equal(qseq_pe_output, Bio::Ngs::Converter.qseq2fastq_pe(@qseq_str))
+  end
+
+  def test_qseq2fastq_se
+    qseq_se_output="@HWI-BRUNOP16X:8:1:15744:1220#0/\nGNAGCCGATCCACCTCCCAGCCTTCCTGGGATACAAGTCTGGCATGACTC\n+\nTBTTTTTTTT`^``Sdbd_deeeeeeeeeeeWUdd_bbbfdTedXOTTTS"    
+    assert_equal(qseq_se_output, Bio::Ngs::Converter.qseq2fastq_se(@qseq_str))
+  end
+
+  
+end
