01618e33f8fc04b751963fa80d4a9c24e43af7e9
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2011-03-04 13:10:45 +0100

    add copyright and original help output

--- lib/bio/appl/ngs/tophat.rb
+++ lib/bio/appl/ngs/tophat.rb
@@ -1,22 +1,94 @@
+#
+#   tophat.rb - description
+#
+# Copyright:: Copyright (C) 2011
+#     Raoul Bonnal <@bioruby.org>
+# License:: The Ruby License
+#
+#
+
+
 #require 'bio/command'
 #require 'shellwords'
 #require 'thor'
 #require 'bio/ngs/utils'
 
+# TopHat maps short sequences from spliced transcripts to whole genomes.
+# 
+# Usage:
+#     tophat [options] <bowtie_index> <reads1[,reads2,...,readsN]> [reads1[,reads2,...,readsN]] [quals1,[quals2,...,qualsN]] [quals1[,quals2,...,qualsN]]
+#     
+# Options:
+#     -v/--version
+#     -o/--output-dir                <string>    [ default: ./tophat_out ]
+#     -a/--min-anchor                <int>       [ default: 8            ]
+#     -m/--splice-mismatches         <0-2>       [ default: 0            ]
+#     -i/--min-intron                <int>       [ default: 50           ]
+#     -I/--max-intron                <int>       [ default: 500000       ]
+#     -g/--max-multihits             <int>       [ default: 40           ]
+#     -F/--min-isoform-fraction      <float>     [ default: 0.15         ]
+#     --solexa-quals                          
+#     --solexa1.3-quals                          (same as phred64-quals)
+#     --phred64-quals                            (same as solexa1.3-quals)
+#     -Q/--quals
+#     --integer-quals
+#     -C/--color                                 (Solid - color space)
+#     --color-out
+#     --library-type                             (--fr-unstranded, --fr-firststrand, --fr-secondstrand, --ff-unstranded, --ff-firststrand, --ff-secondstrand)
+#     -p/--num-threads               <int>       [ default: 1            ]
+#     -G/--GTF                       <filename>
+#     -j/--raw-juncs                 <filename>
+#     -r/--mate-inner-dist           <int>       
+#     --mate-std-dev                 <int>       [ default: 20           ]
+#     --no-novel-juncs                           
+#     --no-gtf-juncs                             
+#     --no-coverage-search
+#     --coverage-search                                              
+#     --no-closure-search
+#     --closure-search
+#     --fill-gaps        
+#     --microexon-search
+#     --butterfly-search
+#     --no-butterfly-search
+#     --keep-tmp
+#     --tmp-dir                      <dirname>
+#     
+# Advanced Options:
+# 
+#     --segment-mismatches           <int>       [ default: 2            ]
+#     --segment-length               <int>       [ default: 25           ]
+#     --min-closure-exon             <int>       [ default: 100          ]
+#     --min-closure-intron           <int>       [ default: 50           ]
+#     --max-closure-intron           <int>       [ default: 5000         ]
+#     --min-coverage-intron          <int>       [ default: 50           ]
+#     --max-coverage-intron          <int>       [ default: 20000        ]
+#     --min-segment-intron           <int>       [ default: 50           ]
+#     --max-segment-intron           <int>       [ default: 500000       ] 
+# 
+# SAM Header Options (for embedding sequencing run metadata in output):
+#     --rg-id                        <string>    (read group ID)
+#     --rg-sample                    <string>    (sample ID)
+#     --rg-library                   <string>    (library ID)
+#     --rg-description               <string>    (descriptive string, no tabs allowed)
+#     --rg-platform-unit             <string>    (e.g Illumina lane ID)
+#     --rg-center                    <string>    (sequencing center name)
+#     --rg-date                      <string>    (ISO 8601 date of the sequencing run)
+#     --rg-platform                  <string>    (Sequencing platform descriptor)  
+# 
+#     for detailed help see http://tophat.cbcb.umd.edu/manual.html
+
 
 module Bio
   module Ngs    
     class Tophat
-      
+
       include Bio::Command::Wrapper
-      
 
       set_program Bio::Ngs::Utils.os_binary("tophat/tophat")
       # User should provide a complete path to the tool.
       # I think it would it better identify the program from just a name
       # looking int othe ext/ or host system path
       # Why not grab the file name from the class name if not specified ?
-
 
       add_option "output-dir",:type => :string, :aliases => '-o'
       add_option "min-anchor", :type => :numeric, :aliases => '-a'
