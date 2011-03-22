77e03894e565f3cbe42285abee23caa9af74ef00
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2011-03-22 12:20:05 +0100

    add test on qseq fixture

--- spec/converter_qseq_spec.rb
+++ spec/converter_qseq_spec.rb
@@ -15,25 +15,27 @@ describe "Converter" do
     it "convert two qseq lines into fastq" do
       qseq = Bio::Ngs::Converter::Qseq.new(:pe)
       qseq.buffer = "H125    98      1       1108    1586    1989    CGATGT  1       CAGA.C.................A.....GAATGGCATGGATCAAGAAAATCCCCCTTGTGAAGAAGAATCAGCAG    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB    0\nH125    98      1       1108    1188    2036    CGATGT  1       CTTGTATGCAGCATCCCCTTCTTGCCTAGGGACTTGAAGGGCCAGGCTTCCTGTCATTGCCTCACTCAAATGTAGC    gggggggggggggegggggffggeggegggeagge^ggdbcgggcdgedegfggffff^ffffefdeeZefccceg    1"
-#      qseq.type = :pe
- # qseq.qseq2fastq_pe(qseq.buffer).should == nil
       fastqs = []
       qseq.to_fastq do |fastq|
         fastqs << fastq if fastq
       end
       fastqs.first.should == "@H125:1:1108:1188:2036#0/1\nCTTGTATGCAGCATCCCCTTCTTGCCTAGGGACTTGAAGGGCCAGGCTTCCTGTCATTGCCTCACTCAAATGTAGC\n+\ngggggggggggggegggggffggeggegggeagge^ggdbcgggcdgedegfggffff^ffffefdeeZefccceg"
     end
-       it "convert a qseq File into fastq for parierd ends" do
-         qseq = Bio::Ngs::Converter::Qseq.new(:pe)
-         qseq.buffer = File.open("/Users/bonnalraoul/Desktop/s_1_1_1108_qseq.txt",'r')
-         fastq_file = File.open("/Users/bonnalraoul/Desktop/s_1_1_1108_qseq.fastq", 'w')
-         fastqs = []
-         qseq.to_fastq do |fastq|
-          fastqs << fastq
-          fastq_file.puts fastq if fastq
-         end
-         fastq_file.close
-         fastqs.compact!.last.should == "@H125:1:1108:20810:200947#0/1\nCTTTTTTTTTTTTTTGTTTTGTTTTGTTTTGTTGATTGTTTGGTTTTTGTGGTTGTGTGTTGTTGTTTTTTTGTTG\n+\ndddWd]_W_BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
-       end    
+    
+    it "convert a qseq File into fastq for parierd ends" do
+      qseq = Bio::Ngs::Converter::Qseq.new(:pe)
+      buffer_filename = File.dirname(__FILE__) + "/fixture/s_1_1_1108_qseq.txt"
+      fastq_filename  = File.dirname(__FILE__) + "/fixture/s_1_1_1108_qseq.fastq"
+      qseq.buffer = File.open(buffer_filename,'r')
+      fastq_file = File.open(fastq_filename, 'w')
+      qseq.to_fastq do |fastq|
+        fastq_file.puts fastq if fastq
+      end
+      fastq_file.close
+      fastq_file = File.open(fastq_filename, 'r')
+      fastq_file.readlines[0..4].join("").should == "@H125:1:1108:1188:2036#0/1\nCTTGTATGCAGCATCCCCTTCTTGCCTAGGGACTTGAAGGGCCAGGCTTCCTGTCATTGCCTCACTCAAATGTAGC\n+\ngggggggggggggegggggffggeggegggeagge^ggdbcgggcdgedegfggffff^ffffefdeeZefccceg\n"
+      fastq_file.close
+      File.delete(fastq_filename)
+    end    
   end
 end
