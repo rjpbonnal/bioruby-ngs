b6d1a89e006bc1d8ef0b49beb2e0ce4f47fad20a
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2011-05-05 21:12:54 +0200

    add SamTools merge and view, using the new feature sub_program

--- lib/bio/appl/ngs/samtools.rb
+++ lib/bio/appl/ngs/samtools.rb
@@ -62,15 +62,27 @@ module Bio
       class View
         include Bio::Command::Wrapper
         set_program Bio::Ngs::Utils.binary("samtools")
-        add_option "output", :type => :string, :aliases => '-o'
+        set_sub_program "view"
+        use_aliases
+        add_option :bam_output, :type => :boolean, :aliases => "-b", :desc => "output BAM", :default => true
+        add_option :print_header_alignment, :type => :boolean, :aliases => "-h", :desc => "print header for the SAM output"
+        add_option :print_header_only, :type => :boolean, :aliases => "-H", :desc => "print header only (no alignments)"
+        add_option :sam_input, :type => :boolean, :aliases => "-S", :desc => "input is SAM"
+        add_option :uncompress, :type => :boolean, :aliases => "-u", :desc => "uncompressed BAM output (force -b)"
+        add_option :compress, :type => :boolean , :aliases => "-1", :desc => "fast compression (force -b)"
+        add_option :flag_hex, :type => :boolean, :aliases => "-x", :desc => "output FLAG in HEX (samtools-C specific)"
+        add_option :flag_string, :type => :boolean, :aliases => "-X", :desc => "output FLAS is string (samtools-C specific)"
+        add_option :output_alignment, :type => :string, :aliases => "-L", :desc => "output alignments overlapping the input BED FILE [null]"
+        add_option :list_ref, :type => :string, :aliases => "-t", :desc => "list of reference names and lengths (force -S) [null]"
+        add_option :ref_sequence, :type => :string, :aliases => "-T", :desc => "reference sequence file (force -S) [null]"
+        add_option :output, :type => :string, :aliases => "-o", :desc => "output file name [stdout]", :required => true
+        add_option :list_group, :type => :string, :aliases => "-R", :desc => "list of read groups to be outputted [null]"
+        add_option :required_flag, :type => :numeric, :aliases => "-f", :desc => "required flag, 0 for unset [0]"
+        add_option :filtering_flag, :type => :numeric, :aliases => "-F", :desc => "filtering flag, 0 for unset [0]"
+        add_option :min_map_qual, :type => :numeric, :aliases => "-q", :desc => "minimum mapping quality [0]"
+        add_option :only_lib_reads, :type => :string, :aliases => "-l", :desc => "only output reads in library STR [null]"
+        add_option :only_grp_reads, :type => :string, :aliases => "r", :desc => "only output reads in read group STR [null]"
 
-        alias :original_run :run
-        def run(opts = {:options=>{}, :arguments=>[], :output_file=>nil, :separator=>"="})
-          opts[:arguments].insert(0, class_name)
-          opts[:arguments].insert(1, "-b")
-          opts[:arguments].insert(2, "-o")
-          original_run(opts)
-        end
       end #View
       
       # Usage:   samtools merge [-nr] [-h inh.sam] <out.bam> <in1.bam> <in2.bam> [...]
@@ -86,16 +98,19 @@ module Bio
       # Note: Samtools' merge does not reconstruct the @RG dictionary in the header. Users
       #       must provide the correct header with -h, or uses Picard which properly maintains
       #       the header dictionary in merging.
+      #out, in1, in2, ... inx Must be passed as arguments
       class Merge
         include Bio::Command::Wrapper
         set_program Bio::Ngs::Utils.binary("samtools")
-        
-        add_option :bams, :type => :array, :required => true
-        alias :original_run :run
-        def run(opts = {:options=>{}, :arguments=>[], :output_file=>nil, :separator=>"="})
-          opts[:arguments].insert(0, class_name)
-          original_run(opts)
-        end
+        set_sub_program "merge"
+        use_aliases
+        add_option :sort_by_read_name, :type => :boolean, :aliases => "-n", :desc => "sort by read names"
+        add_option :attach_rg, :type => :boolean, :aliases => "-r", :desc => "attach RG tag (inferred from file names)"
+        add_option :uncompress, :type => :boolean, :aliases => "-u", :desc => "uncompressed BAM output"
+        add_option :overwrite_output, :type => :boolean, :aliases => "-f", :desc => "overwrite the output BAM if exist"
+        add_option :compress, :type => :boolean , :aliases => "-1", :desc => "compress level 1"
+        add_option :merge_regions, :type => :string, :aliases => "-R", :desc => "merge file in the specified region STR [all]"
+        add_option :copy_header, :type => :string, :aliases => "-h", :desc => "copy the header in FILE to <out.bam> [in1.bam]"
       end #Merge
       
     end #Samtools
