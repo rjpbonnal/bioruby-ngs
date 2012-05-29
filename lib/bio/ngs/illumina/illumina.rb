b5c9854a0674a3a8b639249db7800cc45e931e1f
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2012-05-29 16:06:00 +0200

    search projects in subdirectories as well

--- lib/bio/ngs/illumina/illumina.rb
+++ lib/bio/ngs/illumina/illumina.rb
@@ -40,8 +40,8 @@ module Bio
           projects = Projects.new("Illumina", path)
 
           Dir.chdir(path) do
-            Dir.glob(["Project_*","Undetermined_indices"]).each do |project_dir|
-              project = Project.new(project_dir.sub(/Project_/,""),project_dir)
+            Dir.glob(["**/Project_*","**/Undetermined_indices"]).each do |project_dir|
+              project = Project.new(project_dir.basename.sub(/Project_/,""),project_dir)
               projects.add(project)
               Dir.chdir(project_dir) do
                 Dir.glob("Sample*").each do |sample_dir|
